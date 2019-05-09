f7cd6237622016bfa4f2938599b234dd82bfb0fc
Timo Franz <timo@dumpark.com>
2019-05-09 17:40:56 +0200

    language dropdown with grommet

--- app/containers/LocaleToggle/index.js
+++ app/containers/LocaleToggle/index.js
@@ -4,10 +4,12 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import { Button, DropButton, Text, Box } from 'grommet';
+import { FormDown, FormUp } from 'grommet-icons';
 
 import { appLocales } from 'i18n';
 
@@ -15,29 +17,69 @@ import { getLocale } from 'containers/App/selectors';
 import { LANGUAGES } from 'containers/App/constants';
 import { changeLocale } from 'containers/LanguageProvider/actions';
 
-import Toggle from 'components/Toggle';
-
 const Styled = styled.span`
-  padding: 2px;
+  padding: 0 5px;
 `;
 
+const Option = styled(Button)``;
+
+const DropContent = ({ active, options, onSelect }) => (
+  <Box pad="small">
+    {options &&
+      options.map(option => (
+        <Option
+          plain
+          color={active === option ? 'dark-4' : 'dark-1'}
+          onClick={() => onSelect(option)}
+        >
+          {LANGUAGES.long[option]}
+        </Option>
+      ))}
+  </Box>
+);
+
 export function LocaleToggle(props) {
+  const [open, setOpen] = useState(false);
   return (
     <Styled>
-      <Toggle
-        inverse
-        value={props.locale}
-        values={appLocales}
-        formattedMessages={LANGUAGES.short}
-        onToggle={props.onLocaleToggle}
-      />
+      <DropButton
+        plain
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        dropContent={
+          <DropContent
+            active={props.locale}
+            options={appLocales}
+            onSelect={props.onLocaleToggle}
+          />
+        }
+        dropProps={{ align: { top: 'bottom', right: 'right' } }}
+      >
+        <Text>{LANGUAGES.short[props.locale]}</Text>
+        {!open && <FormDown />}
+        {open && <FormUp />}
+      </DropButton>
     </Styled>
   );
 }
 
+// <Toggle
+//   inverse
+//   value={props.locale}
+//   values={appLocales}
+//   formattedMessages={LANGUAGES.short}
+//   onToggle={props.onLocaleToggle}
+// />
+
 LocaleToggle.propTypes = {
   onLocaleToggle: PropTypes.func,
   locale: PropTypes.string,
+};
+
+DropContent.propTypes = {
+  onSelect: PropTypes.func,
+  active: PropTypes.string,
+  options: PropTypes.array,
 };
 
 const mapStateToProps = state => ({
@@ -46,7 +88,7 @@ const mapStateToProps = state => ({
 
 export function mapDispatchToProps(dispatch) {
   return {
-    onLocaleToggle: evt => dispatch(changeLocale(evt.target.value)),
+    onLocaleToggle: locale => dispatch(changeLocale(locale)),
     dispatch,
   };
 }
