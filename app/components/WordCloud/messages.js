5569463506898f09d3eb59b77b6c39c4dd2aebd4
Timo Franz <timo@dumpark.com>
2019-05-23 22:03:38 +0200

    add tooltip for word cloud

--- app/components/WordCloud/messages.js
+++ app/components/WordCloud/messages.js
@@ -9,6 +9,11 @@ import { defineMessages } from 'react-intl';
 export const scope = 'hrmi.components.WordCloud';
 
 export default defineMessages({
+  tooltip: {
+    id: `${scope}.tooltip`,
+    defaultMessage:
+      'These numbers show the percentage of our human rights experts who identified these people as being most at risk of having this right violated.',
+  },
   noGroupData: {
     id: `${scope}.noGroupData`,
     defaultMessage: 'No data',
