1e60af1e9c5cacb50612eb0c2aaefb5050bc4174
Timo Franz <timo@dumpark.com>
2019-09-24 15:06:57 +0200

    stack "How To Read content" to allow for longer benchmark labels #73

--- app/containers/HowToRead/HTRSummaryRights.js
+++ app/containers/HowToRead/HTRSummaryRights.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import { FormattedMessage, intlShape, injectIntl } from 'react-intl';
 import styled from 'styled-components';
-import { Text, Heading, Box } from 'grommet';
+import { Text, Heading, Box, ResponsiveContext } from 'grommet';
 
 import { DIMENSIONS } from 'containers/App/constants';
 import { rightsForDimension } from 'utils/rights';
@@ -25,135 +25,154 @@ const StyledLI = styled.li`
 
 const randomValue = (min, max) => Math.random() * (max - min) + min;
 
+const stackContent = size => size === 'large' || size === 'small';
+
 function HTRSummaryRights({ intl }) {
   return (
-    <Styled>
-      <HTRParagraph>
-        <FormattedMessage {...messages.summary.rights.intro} />
-      </HTRParagraph>
-      {DIMENSIONS.map(d => {
-        const rights = rightsForDimension(d.key);
-        const dataMultiple = {
-          color: d.key,
-          unit: d.type === 'esr' ? '%' : '',
-          maxValue: d.type === 'cpr' ? 10 : 100,
-          data: rights.map(right => ({
-            key: right.key,
-            value: d.type === 'cpr' ? randomValue(5, 10) : randomValue(50, 95),
-            refValues: d.type === 'esr' && [
-              { value: 100, style: 'solid', key: 'best' },
-            ],
-          })),
-        };
-        return (
-          <Box key={d.key} margin={{ bottom: 'medium' }} responsive={false}>
-            <Heading
-              responsive={false}
-              level={4}
-              margin={{
-                bottom: 'none',
-                top: 'xsmall',
-              }}
-            >
-              <FormattedMessage {...rootMessages.dimensions[d.key]} />
-            </Heading>
-            <Box
-              direction="row"
-              align="start"
-              pad={{ top: 'small' }}
-              responsive={false}
-            >
-              <Box
-                width="50%"
-                flex={{ shrink: 0 }}
-                pad={{ left: 'small', right: 'medium' }}
-                responsive={false}
-              >
-                <Box
-                  style={{
-                    position: 'relative',
-                    marginTop: d.type === 'esr' ? '26px' : 0,
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Styled>
+          <HTRParagraph>
+            <FormattedMessage {...messages.summary.rights.intro} />
+          </HTRParagraph>
+          {DIMENSIONS.map(d => {
+            const rights = rightsForDimension(d.key);
+            const dataMultiple = {
+              color: d.key,
+              unit: d.type === 'esr' ? '%' : '',
+              maxValue: d.type === 'cpr' ? 10 : 100,
+              data: rights.map(right => ({
+                key: right.key,
+                value:
+                  d.type === 'cpr' ? randomValue(5, 10) : randomValue(50, 95),
+                refValues: d.type === 'esr' && [
+                  { value: 100, style: 'solid', key: 'best' },
+                ],
+              })),
+            };
+            return (
+              <Box key={d.key} margin={{ bottom: 'medium' }} responsive={false}>
+                <Heading
+                  responsive={false}
+                  level={4}
+                  margin={{
+                    bottom: 'none',
+                    top: 'xsmall',
                   }}
-                  margin={{ bottom: 'medium' }}
+                >
+                  <FormattedMessage {...rootMessages.dimensions[d.key]} />
+                </Heading>
+                <Box
+                  direction={stackContent(size) ? 'column' : 'row'}
+                  align="start"
+                  pad={{ top: 'small' }}
                   responsive={false}
                 >
-                  {d.type === 'esr' && (
-                    <AnnotateBenchmark
-                      tooltip={false}
-                      label={intl.formatMessage(
-                        rootMessages.settings.benchmark.nameShort,
+                  <Box
+                    width={stackContent(size) ? '100%' : '50%'}
+                    flex={{ shrink: 0 }}
+                    pad={
+                      stackContent(size)
+                        ? { right: 'small' }
+                        : { left: 'small', right: 'medium' }
+                    }
+                    responsive={false}
+                  >
+                    <Box
+                      style={{
+                        position: 'relative',
+                        marginTop: d.type === 'esr' ? '26px' : 0,
+                      }}
+                      margin={{ bottom: 'medium' }}
+                      responsive={false}
+                    >
+                      {d.type === 'esr' && (
+                        <AnnotateBenchmark
+                          tooltip={false}
+                          label={intl.formatMessage(
+                            rootMessages.settings.benchmark.nameShort,
+                          )}
+                          above
+                          align="right"
+                          relative
+                          left={100}
+                          margin="1px"
+                        />
                       )}
-                      above
-                      align="right"
-                      relative
-                      left={100}
-                      margin="1px"
-                    />
-                  )}
-                  <BarMultiple
-                    dataMultiple={dataMultiple}
-                    showLabels
-                    totalHeight={36}
-                  />
-                  <AnnotateBetterWorse absolute />
+                      <BarMultiple
+                        dataMultiple={dataMultiple}
+                        showLabels
+                        totalHeight={36}
+                      />
+                      <AnnotateBetterWorse absolute />
+                    </Box>
+                  </Box>
+                  <Box
+                    width={stackContent(size) ? '100%' : '50%'}
+                    flex={{ shrink: 0 }}
+                    pad={stackContent(size) ? 'none' : { left: 'medium' }}
+                  >
+                    <HTRParagraph>
+                      <FormattedMessage {...messages.summary.rights[d.key]} />
+                    </HTRParagraph>
+                  </Box>
                 </Box>
-              </Box>
-              <Box width="50%" flex={{ shrink: 0 }} pad={{ left: 'medium' }}>
-                <HTRParagraph>
-                  <FormattedMessage {...messages.summary.rights[d.key]} />
-                </HTRParagraph>
-              </Box>
-            </Box>
-            <Heading
-              responsive={false}
-              level={6}
-              margin={{ top: 'xsmall', bottom: '0' }}
-            >
-              <FormattedMessage {...messages.summary.rights.rightsListTitle} />
-            </Heading>
-            <StyledUL>
-              {rights.map(r => (
-                <StyledLI key={r.key}>
-                  <Text size="xsmall">
-                    <FormattedMessage {...rootMessages.rights[r.key]} />
-                  </Text>
-                </StyledLI>
-              ))}
-            </StyledUL>
-            {d.key === 'esr' && (
-              <Box margin={{ top: 'small' }} responsive={false}>
-                <HTRParagraph>
-                  <FormattedMessage {...messages.general.benchmarkIntro} />
-                  <span style={{ fontWeight: 600, margin: '0 3px' }}>
-                    <FormattedMessage
-                      {...rootMessages.settings.benchmark.name}
-                    />
-                  </span>
-                </HTRParagraph>
-                <HTRParagraph margin={{ vertical: 'xsmall' }}>
-                  <span style={{ fontWeight: 600 }}>
-                    {`${intl.formatMessage(
-                      rootMessages.settings.benchmark.adjusted,
-                    )}: `}
-                  </span>
+                <Heading
+                  responsive={false}
+                  level={6}
+                  margin={{ top: 'xsmall', bottom: '0' }}
+                >
                   <FormattedMessage
-                    {...rootMessages.tooltip.benchmark.adjusted}
+                    {...messages.summary.rights.rightsListTitle}
                   />
-                </HTRParagraph>
-                <HTRParagraph margin={{ vertical: 'xsmall' }}>
-                  <span style={{ fontWeight: 600 }}>
-                    {`${intl.formatMessage(
-                      rootMessages.settings.benchmark.best,
-                    )}: `}
-                  </span>
-                  <FormattedMessage {...rootMessages.tooltip.benchmark.best} />
-                </HTRParagraph>
+                </Heading>
+                <StyledUL>
+                  {rights.map(r => (
+                    <StyledLI key={r.key}>
+                      <Text size="xsmall">
+                        <FormattedMessage {...rootMessages.rights[r.key]} />
+                      </Text>
+                    </StyledLI>
+                  ))}
+                </StyledUL>
+                {d.key === 'esr' && (
+                  <Box margin={{ top: 'small' }} responsive={false}>
+                    <HTRParagraph>
+                      <FormattedMessage {...messages.general.benchmarkIntro} />
+                      <span style={{ fontWeight: 600, margin: '0 3px' }}>
+                        <FormattedMessage
+                          {...rootMessages.settings.benchmark.name}
+                        />
+                      </span>
+                    </HTRParagraph>
+                    <HTRParagraph margin={{ vertical: 'xsmall' }}>
+                      <span style={{ fontWeight: 600 }}>
+                        {`${intl.formatMessage(
+                          rootMessages.settings.benchmark.adjusted,
+                        )}: `}
+                      </span>
+                      <FormattedMessage
+                        {...rootMessages.tooltip.benchmark.adjusted}
+                      />
+                    </HTRParagraph>
+                    <HTRParagraph margin={{ vertical: 'xsmall' }}>
+                      <span style={{ fontWeight: 600 }}>
+                        {`${intl.formatMessage(
+                          rootMessages.settings.benchmark.best,
+                        )}: `}
+                      </span>
+                      <FormattedMessage
+                        {...rootMessages.tooltip.benchmark.best}
+                      />
+                    </HTRParagraph>
+                  </Box>
+                )}
               </Box>
-            )}
-          </Box>
-        );
-      })}
-    </Styled>
+            );
+          })}
+        </Styled>
+      )}
+    </ResponsiveContext.Consumer>
   );
 }
 
