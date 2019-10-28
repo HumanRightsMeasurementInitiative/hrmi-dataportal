c13ddb5ad434a1ca16979d12b45c8691376c9c94
Timo Franz <timo@dumpark.com>
2019-10-28 14:27:18 +0100

    add word cloud hover #85

---
+++ app/components/WordCloud/Word.js
@@ -0,0 +1,160 @@
+/**
+ *
+ * Word
+ *
+ */
+
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { intlShape, injectIntl, FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
+import { Box, Drop, Button, ResponsiveContext, Text } from 'grommet';
+import { scaleLinear } from 'd3-scale';
+import Tooltip from 'components/Tooltip';
+import rootMessages from 'messages';
+import messages from './messages';
+
+const Tag = styled(Box)`
+  opacity: ${props => props.opacity || 1};
+  font-size: ${props => props.size}px;
+  line-height: ${props => props.size}px;
+  color: ${props => props.theme.global.colors[props.color]};
+  font-weight: ${props => props.weight || 400};
+`;
+
+const StyledDrop = styled(Drop)`
+  margin: 0 0 13px;
+  overflow: visible;
+  font-size: ${({ theme }) => theme.text.xsmall.size};
+  line-height: ${({ theme }) => theme.text.xsmall.height};
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 100%;
+    width: 0;
+    height: 0;
+    border-top: 8px solid ${props => props.theme.global.colors['dark-1']};
+    border-left: 8px solid transparent;
+    border-right: 8px solid transparent;
+    margin: 0 auto;
+  }
+`;
+
+const MAX_SIZE = 42;
+const MIN_SIZE = 14;
+const MAX_SIZE_MOBILE = 33;
+const MIN_SIZE_MOBILE = 11;
+
+const scaleFontWeight = value => {
+  if (value >= 0.9) return 900;
+  if (value >= 0.7) return 700;
+  if (value >= 0.6) return 600;
+  return 400;
+};
+
+const scaleFont = scaleLinear()
+  .domain([0, 1])
+  .range([MIN_SIZE / MAX_SIZE, 1]);
+const scaleFontMobile = scaleLinear()
+  .domain([0, 1])
+  .range([MIN_SIZE_MOBILE / MAX_SIZE_MOBILE, 1]);
+
+const scaleOpacity = scaleLinear()
+  .domain([0, 1])
+  .range([0.66, 1]);
+
+export function Word({ score, tooltip, dimension, intl }) {
+  const [over, setOver] = useState(false);
+  const [open, setOpen] = useState(false);
+  const button = useRef(null);
+  return (
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Box direction="row" align="center" key={score.people_code}>
+          <Tag
+            key={score.people_code}
+            weight={scaleFontWeight(score.proportion)}
+            opacity={over || open ? 1 : scaleOpacity(score.proportion)}
+            size={
+              size === 'small'
+                ? scaleFontMobile(score.proportion) * MAX_SIZE_MOBILE
+                : scaleFont(score.proportion) * MAX_SIZE
+            }
+            color={`${dimension}Cloud`}
+            direction="row"
+            align="center"
+          >
+            <Button
+              ref={button}
+              onClick={evt => {
+                if (evt) evt.preventDefault();
+                if (evt) evt.stopPropagation();
+                setOpen(!open);
+              }}
+              onMouseEnter={() => setOver(true)}
+              onMouseLeave={() => setOver(false)}
+              onFocus={() => setOver(true)}
+              onBlur={() => setOver(false)}
+              margin={{ horizontal: 'xsmall' }}
+              style={{
+                WebkitAppearance: 'none',
+                MozAppearance: 'none',
+              }}
+            >
+              <FormattedMessage
+                {...rootMessages['people-at-risk'][score.people_code]}
+              />
+            </Button>
+            {(over || open) && button.current && (
+              <StyledDrop
+                align={{ bottom: 'top' }}
+                stretch={false}
+                elevation="small"
+                target={button.current}
+                onClickOutside={() => setOpen(false)}
+              >
+                <Box
+                  pad={{ vertical: 'small', horizontal: 'small' }}
+                  background="dark-1"
+                  style={{ maxWidth: '200px' }}
+                  onClick={evt => {
+                    if (evt) evt.preventDefault();
+                    if (evt) evt.stopPropagation();
+                  }}
+                  align="start"
+                >
+                  <Text size="large">
+                    {`${Math.round(100 * score.proportion)}%`}
+                  </Text>
+                  <Text>
+                    {`of our human rights experts identified "${intl.formatMessage(
+                      rootMessages['people-at-risk'][score.people_code],
+                    )}" as being at risk of having this right violated.`}
+                  </Text>
+                </Box>
+              </StyledDrop>
+            )}
+          </Tag>
+          {tooltip && (
+            <Tooltip
+              iconSize="large"
+              text={intl.formatMessage(messages.tooltip)}
+            />
+          )}
+        </Box>
+      )}
+    </ResponsiveContext.Consumer>
+  );
+}
+
+Word.propTypes = {
+  score: PropTypes.object,
+  dimension: PropTypes.string,
+  tooltip: PropTypes.bool,
+  border: PropTypes.bool,
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(Word);
