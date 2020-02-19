94262df075c1368921b57477a0663537f01268c1
Timo Franz <timo@dumpark.com>
2020-02-19 15:08:50 +1300

    fix ie11 tooltip overflow #115

--- app/components/Tooltip/index.js
+++ app/components/Tooltip/index.js
@@ -4,6 +4,8 @@ import { Box, Drop, Button, ResponsiveContext, Layer } from 'grommet';
 import { CircleInformation, Close } from 'grommet-icons';
 import styled from 'styled-components';
 
+import ContentWrap from './ContentWrap';
+
 const StyledDrop = styled(Drop)`
   margin: 0 0 13px;
   overflow: visible;
@@ -35,20 +37,22 @@ const renderContent = (maxWidth, component, text, onClose, inModal) => (
     }}
     align={inModal ? 'center' : 'start'}
   >
-    {onClose && (
-      <Button
-        onClick={evt => {
-          if (evt) evt.preventDefault();
-          if (evt) evt.stopPropagation();
-          onClose();
-        }}
-        margin={{ left: 'auto' }}
-      >
-        <Close color="white" size="large" />
-      </Button>
-    )}
-    {component}
-    {!component && <span>{text}</span>}
+    <ContentWrap>
+      {onClose && (
+        <Button
+          onClick={evt => {
+            if (evt) evt.preventDefault();
+            if (evt) evt.stopPropagation();
+            onClose();
+          }}
+          margin={{ left: 'auto' }}
+        >
+          <Close color="white" size="large" />
+        </Button>
+      )}
+      {component}
+      {!component && <span>{text}</span>}
+    </ContentWrap>
   </Box>
 );
 
