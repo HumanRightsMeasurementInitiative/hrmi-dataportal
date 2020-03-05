3d185f14dd77cf5e39bdc76e10ba3c29c0d71481
Timo Franz <timo@dumpark.com>
2020-03-05 14:06:58 +1300

    header mobile v3

--- app/containers/Header/index.js
+++ app/containers/Header/index.js
@@ -11,7 +11,15 @@ import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import styled, { withTheme } from 'styled-components';
-import { Box, Button, Drop, ResponsiveContext, Image, Text } from 'grommet';
+import {
+  Box,
+  Button,
+  Drop,
+  ResponsiveContext,
+  Image,
+  Text,
+  Layer,
+} from 'grommet';
 import { Menu, Close, FormDown, FormUp } from 'grommet-icons';
 
 import { getRouterMatch, getRouterRoute } from 'containers/App/selectors';
@@ -20,6 +28,7 @@ import ButtonNavPrimary from 'styled/ButtonNavPrimary';
 import { isMinSize, isMaxSize } from 'utils/responsive';
 
 import logo from 'images/HRMI-Logo-HOR-RGB-x2.png';
+import logoS from 'images/HRMI-Logo-HOR-RGB-small-x2.png';
 
 import LocaleToggle from 'containers/LocaleToggle';
 import { appLocales } from 'i18n';
@@ -53,9 +62,7 @@ const NavBarTop = props => (
     direction="row"
     align="center"
     justify="end"
-    background="white"
     height={`${props.theme.sizes.header.heightTop}px`}
-    fill="horizontal"
     pad={{ horizontal: 'small' }}
     {...props}
   />
@@ -68,9 +75,7 @@ const NavBarBottom = props => (
   <Box
     direction="row"
     align="end"
-    justify="end"
-    background="white"
-    fill="horizontal"
+    justify={props.size === 'small' ? 'start' : 'end'}
     height={`${props.theme.sizes.header.heightBottom}px`}
     pad={{ horizontal: 'small' }}
     {...props}
@@ -78,25 +83,33 @@ const NavBarBottom = props => (
 );
 NavBarBottom.propTypes = {
   theme: PropTypes.object,
+  size: PropTypes.string,
 };
 
 const BrandButton = styled(Button)`
-  height: ${({ theme }) => theme.sizes.header.height}px;
-  width: ${({ theme }) => theme.sizes.header.brandWidth}px;
+  width: ${({ theme }) => theme.sizes.header.small.brandWidth}px;
   &:hover {
     opacity: 0.8;
   }
   @media (min-width: ${({ theme }) => theme.breakpointsMin.medium}) {
+    height: ${({ theme }) => theme.sizes.header.height}px;
     width: ${({ theme }) => theme.sizes.header.brandWidth}px;
   }
 `;
 // color: ${({ theme }) => theme.global.colors.hover};
 const BrandInner = styled(Box)`
-  height: ${({ theme }) => theme.sizes.header.height}px;
-  padding-top: ${({ theme }) => theme.sizes.header.padTop}px;
-  padding-bottom: ${({ theme }) => theme.sizes.header.padBottom - 2}px;
-  padding-left: ${({ theme }) => theme.sizes.header.padLeft}px;
-  padding-right: ${({ theme }) => theme.sizes.header.padLeft}px;
+  padding-top: ${({ theme }) => theme.sizes.header.small.padTop}px;
+  padding-bottom: ${({ theme }) => theme.sizes.header.small.padBottom - 2}px;
+  padding-left: ${({ theme }) => theme.sizes.header.small.padLeft}px;
+  padding-right: ${({ theme }) => theme.sizes.header.small.padLeft}px;
+  height: ${({ theme }) => theme.sizes.header.small.heightTop}px;
+  @media (min-width: ${({ theme }) => theme.breakpointsMin.medium}) {
+    height: ${({ theme }) => theme.sizes.header.height}px;
+    padding-top: ${({ theme }) => theme.sizes.header.padTop}px;
+    padding-bottom: ${({ theme }) => theme.sizes.header.padBottom - 2}px;
+    padding-left: ${({ theme }) => theme.sizes.header.padLeft}px;
+    padding-right: ${({ theme }) => theme.sizes.header.padLeft}px;
+  }
 `;
 
 const LogoWrap = styled(Box)``;
@@ -105,9 +118,14 @@ const Logo = styled(Image)``;
 const TitleWrap = styled(Box)`
   text-transform: uppercase;
   font-weight: bold;
-  margin-top: -5px;
-  font-size: 19px;
-  line-height: 30px;
+  margin-top: -1px;
+  font-size: 15px;
+  line-height: 18px;
+  @media (min-width: ${({ theme }) => theme.breakpointsMin.medium}) {
+    margin-top: -5px;
+    font-size: 19px;
+    line-height: 30px;
+  }
 `;
 
 // prettier-ignore
@@ -119,35 +137,19 @@ const TitleWrap = styled(Box)`
 // width: 100%;
 // background: ${props => props.theme.global.colors['dark-2']};
 const MenuList = styled(Box)`
-  z-index: 99999;
   padding: ${({ theme }) => theme.global.edgeSize.medium} 0;
-  overflow: auto;
-  @media (min-width: ${({ theme }) => theme.breakpointsMin.large}) {
-    padding: 0;
-    position: relative;
-    z-index: 300;
-  }
 `;
 
 const MenuGroup = styled(Box)`
   vertical-align: top;
-  @media (min-width: ${({ theme }) => theme.breakpointsMin.large}) {
-  }
 `;
 // prettier-ignore
 const ToggleMenu = styled(Button)`
-  display: block;
-  position: absolute;
-  right: 0;
-  top: 0;
   z-index: 300;
   width: 44px;
   height: 44px;
   background-color: transparent;
   text-align: center;
-  @media (min-width: ${({ theme }) => theme.breakpointsMin.large}) {
-    display: none;
-  }
 `;
 
 const SearchWrap = styled(Box)`
@@ -155,7 +157,7 @@ const SearchWrap = styled(Box)`
 `;
 // prettier-ignore
 const ButtonNavSecondary = styled(Button)`
-  margin: 0px 10px;
+  margin: 0px 5px;
   padding-left: 3px;
   font-weight: 600;
   height: ${({ theme }) => theme.sizes.header.heightBottom - 4}px;
@@ -167,6 +169,9 @@ const ButtonNavSecondary = styled(Button)`
   border-color: ${({ theme, subject, active }) => (
     (active) ? theme.global.colors[subject] : 'transparent'
   )};
+  &:first-child {
+    margin: 0;
+  }
   &:hover {
     color: ${({ theme, subject }) => theme.global.colors[subject]};
   }
@@ -176,7 +181,7 @@ const ButtonNavSecondary = styled(Button)`
 `;
 
 const ButtonSecondary = React.forwardRef(
-  ({ active, open, onClick, label, subject }, ref) => (
+  ({ active, open, onClick, label, subject, size }, ref) => (
     <ButtonNavSecondary
       plain
       active={active}
@@ -191,14 +196,22 @@ const ButtonSecondary = React.forwardRef(
           align="center"
           justify="between"
           fill="horizontal"
-          gap="xsmall"
+          gap={size === 'small' ? 'xxsmall' : 'xsmall'}
         >
-          <Text size="large">
+          <Text size={size === 'small' ? 'medium' : 'large'}>
             <FormattedMessage {...rootMessages.labels[label]} />
           </Text>
-          {open && <FormUp size="xlarge" style={{ stroke: 'currentColor' }} />}
+          {open && (
+            <FormUp
+              size={size === 'small' ? 'large' : 'xlarge'}
+              style={{ stroke: 'currentColor' }}
+            />
+          )}
           {!open && (
-            <FormDown size="xlarge" style={{ stroke: 'currentColor' }} />
+            <FormDown
+              size={size === 'small' ? 'large' : 'xlarge'}
+              style={{ stroke: 'currentColor' }}
+            />
           )}
         </Box>
       }
@@ -212,7 +225,26 @@ ButtonSecondary.propTypes = {
   onClick: PropTypes.func,
   label: PropTypes.string,
   subject: PropTypes.string,
+  size: PropTypes.string,
 };
+
+const renderPages = (match, nav, setShowMenu) =>
+  PAGES &&
+  Object.values(PAGES)
+    .filter(page => page.primary)
+    .map(page => (
+      <ButtonNavPrimary
+        key={page.key}
+        active={page.key === match}
+        disabled={page.key === match}
+        onClick={() => {
+          if (setShowMenu) setShowMenu(false);
+          nav(`${PATHS.PAGE}/${page.key}`);
+        }}
+      >
+        <FormattedMessage {...rootMessages.page[page.key]} />
+      </ButtonNavPrimary>
+    ));
 
 const DEPENDENCIES = ['countries'];
 
@@ -230,82 +262,86 @@ export function Header({ nav, onLoadData, match, path, theme, intl }) {
   const [showMetrics, setShowMetrics] = useState(false);
   const countryTarget = useRef(null);
   const metricTarget = useRef(null);
+  const menuRef = useRef(null);
 
   const onHome = () => {
     setShowMenu(false);
     nav({ pathname: '', search: '' });
   };
-  const showSecondary = true;
   return (
     <ResponsiveContext.Consumer>
       {size => (
-        <Styled role="banner" showSecondary={showSecondary}>
-          <Box direction="row" elevation="medium" background="white">
-            <BrandButton plain onClick={onHome}>
-              <BrandInner
-                direction="row"
-                justify="start"
-                gap="10px"
-                fill="false"
-              >
-                <LogoWrap
-                  justify="start"
-                  width={`${theme.sizes.header.logoWidth}px`}
-                  flex={{ shrink: false }}
-                >
-                  <Logo
-                    src={logo}
-                    alt={`${intl.formatMessage(messages.appTitle)}`}
-                    a11yTitle={`${intl.formatMessage(messages.appTitle)}`}
-                    fit="contain"
-                  />
-                </LogoWrap>
-                <TitleWrap>
-                  <FormattedMessage {...messages.appTitle} />
-                </TitleWrap>
-              </BrandInner>
-            </BrandButton>
-            <Box fill>
-              <NavBarTop theme={theme}>
-                {appLocales.length > 1 && isMaxSize(size, 'medium') && (
-                  <Box margin={{ left: 'auto', right: 'large' }}>
+        <Styled role="banner">
+          <Box elevation="medium" background="white">
+            <Box direction={size === 'small' ? 'column' : 'row'}>
+              <Box direction="row" align="center">
+                <BrandButton plain onClick={onHome}>
+                  <BrandInner
+                    direction="row"
+                    justify="start"
+                    gap="10px"
+                    fill="false"
+                  >
+                    <LogoWrap
+                      justify="start"
+                      width={`${theme.sizes.header.logoWidth}px`}
+                      flex={{ shrink: false }}
+                    >
+                      <Logo
+                        src={size === 'small' ? logoS : logo}
+                        alt={`${intl.formatMessage(messages.appTitle)}`}
+                        a11yTitle={`${intl.formatMessage(messages.appTitle)}`}
+                        fit="contain"
+                      />
+                    </LogoWrap>
+                    <TitleWrap>
+                      <FormattedMessage {...messages.appTitle} />
+                    </TitleWrap>
+                  </BrandInner>
+                </BrandButton>
+                {size === 'small' && appLocales.length > 1 && (
+                  <Box margin={{ left: 'auto' }}>
                     <LocaleToggle />
                   </Box>
                 )}
-                <ToggleMenu plain onClick={() => setShowMenu(!showMenu)}>
-                  {!showMenu && <Menu />}
-                  {showMenu && <Close />}
-                </ToggleMenu>
-                <MenuList visible={showMenu} direction="row">
-                  <MenuGroup direction="row">
-                    {PAGES &&
-                      Object.values(PAGES)
-                        .filter(page => page.primary)
-                        .map(page => (
-                          <ButtonNavPrimary
-                            key={page.key}
-                            active={page.key === match}
-                            disabled={page.key === match}
-                            onClick={() => {
-                              setShowMenu(false);
-                              nav(`${PATHS.PAGE}/${page.key}`);
-                            }}
-                          >
-                            <FormattedMessage
-                              {...rootMessages.page[page.key]}
-                            />
-                          </ButtonNavPrimary>
-                        ))}
-                  </MenuGroup>
-                  {appLocales.length > 1 && isMinSize(size, 'large') && (
-                    <MenuGroup direction="row">
+                {size === 'small' && (
+                  <ToggleMenu
+                    plain
+                    onClick={() => setShowMenu(!showMenu)}
+                    ref={menuRef}
+                  >
+                    {!showMenu && <Menu color="dark-3" />}
+                    {showMenu && <Close color="dark-3" />}
+                  </ToggleMenu>
+                )}
+                {showMenu && size === 'small' && (
+                  <Layer
+                    full="horizontal"
+                    margin={{ top: '50px' }}
+                    onClickOutside={() => setShowMenu(false)}
+                    responsive={false}
+                    position="top"
+                    modal={false}
+                    animate={false}
+                  >
+                    <MenuList elevation="large">
+                      <MenuGroup>
+                        {renderPages(match, nav, setShowMenu)}
+                      </MenuGroup>
+                    </MenuList>
+                  </Layer>
+                )}
+              </Box>
+              <Box fill>
+                {isMinSize(size, 'medium') && (
+                  <NavBarTop theme={theme} direction="row" justify="end">
+                    {renderPages(match, nav)}
+                    {appLocales.length > 1 && isMinSize(size, 'medium') && (
                       <LocaleToggle />
-                    </MenuGroup>
-                  )}
-                </MenuList>
-              </NavBarTop>
-              {showSecondary && (
-                <NavBarBottom theme={theme}>
+                    )}
+                  </NavBarTop>
+                )}
+                <NavBarBottom theme={theme} size={size}>
                   {(!showSearch || isMaxSize(size, 'small')) && (
                     <>
                       <ButtonSecondary
@@ -320,6 +356,7 @@ export function Header({ nav, onLoadData, match, path, theme, intl }) {
                         label="countries"
                         ref={countryTarget}
                         subject="countries"
+                        size={size}
                       />
                       {showCountries && size === 'small' && (
                         <NavCountry
@@ -351,6 +388,7 @@ export function Header({ nav, onLoadData, match, path, theme, intl }) {
                         label="metrics"
                         subject="metrics"
                         ref={metricTarget}
+                        size={size}
                       />
                       {showMetrics && size === 'small' && (
                         <NavMetric
@@ -380,8 +418,9 @@ export function Header({ nav, onLoadData, match, path, theme, intl }) {
                         onClick={() => {
                           nav('page/at-risk/');
                         }}
+                        size={size}
                       >
-                        <Text size="large">
+                        <Text size={size === 'small' ? 'medium' : 'large'}>
                           <FormattedMessage {...rootMessages.page['at-risk']} />
                         </Text>
                       </ButtonNavSecondary>
@@ -397,7 +436,7 @@ export function Header({ nav, onLoadData, match, path, theme, intl }) {
                     </SearchWrap>
                   )}
                 </NavBarBottom>
-              )}
+              </Box>
             </Box>
           </Box>
         </Styled>
