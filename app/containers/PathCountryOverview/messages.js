b348e0c2a9cf682a6dd4e8feace8a6b7bfcbc3bf
Timo Franz <timo@dumpark.com>
2020-03-09 17:29:22 +1300

    country overview text

--- app/containers/PathCountryOverview/messages.js
+++ app/containers/PathCountryOverview/messages.js
@@ -6,11 +6,23 @@
 
 import { defineMessages } from 'react-intl';
 
-export const scope = 'hrmi.components.CountryOverview';
+export const scope = 'hrmi.containers.PathCountryOverview';
 
 export default defineMessages({
   title: {
     id: `${scope}.title`,
-    defaultMessage: 'Human rights performance overview by country',
+    defaultMessage: '{no} countries',
+  },
+  header: {
+    d: {
+      id: `${scope}.header.d`,
+      defaultMessage:
+        'Compare how countries perform across the three human rights categories: {esrLink}, {physintLink} and {empowerLink}',
+    },
+    r: {
+      id: `${scope}.header.r`,
+      defaultMessage:
+        'Compare how countries perform across the {no} human rights of our three categories: {esrLink}, {physintLink} and {empowerLink}',
+    },
   },
 });
