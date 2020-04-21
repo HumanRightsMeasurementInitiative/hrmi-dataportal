66df8380c626957ae50f3024c1c21879d521f1de
Timo Franz <timo@dumpark.com>
2020-04-22 00:56:40 +0200

    align left

--- app/components/HINote/index.js
+++ app/components/HINote/index.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 
-import styled, { withTheme } from 'styled-components';
+import styled from 'styled-components';
 import ContentMaxWidth from 'styled/ContentMaxWidth';
 import ContentContainer from 'styled/ContentContainer';
-import { Box, Text, ResponsiveContext } from 'grommet';
-
-import { getFloatingAsideWidth, getWindowDimensions } from 'utils/responsive';
+import Aside from 'components/Aside';
+import { Box, Text } from 'grommet';
 
 import rootMessages from 'messages';
 import messages from './messages';
@@ -16,62 +15,53 @@ const Styled = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
-  right: ${({ rightPosition }) => rightPosition}px;
-  background: white;
+  right: 0;
   z-index: 14;
-  box-shadow: 0px -10px 5px -5px rgba(255, 255, 255, 1);
+  background: ${({ hasAside }) => (hasAside ? 'transparent' : 'white')};
+  box-shadow: ${({ hasAside }) =>
+    hasAside ? '0' : '-5px -10px 5px -5px rgba(255, 255, 255, 1)'};
+`;
+const StyledBox = styled(Box)`
+  background: ${({ hasAside }) => (!hasAside ? 'transparent' : 'white')};
+  box-shadow: ${({ hasAside }) =>
+    !hasAside ? '0' : '-5px -10px 5px -5px rgba(255, 255, 255, 1)'};
+  padding-right: ${({ hasAside, theme }) =>
+    hasAside ? theme.global.edgeSize.xlarge : 0};
 `;
 
-function HINote({ intl, hasAside, theme }) {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions(),
-  );
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+function HINote({ intl, hasAside }) {
   return (
-    <ResponsiveContext.Consumer>
-      {size => (
-        <Styled
-          rightPosition={
-            hasAside ? getFloatingAsideWidth(size, theme, windowDimensions) : 0
-          }
-        >
-          <ContentContainer direction="column">
-            <ContentMaxWidth>
-              <Box
-                pad={{ bottom: 'xsmall', top: 'xsmall' }}
-                align="end"
-                fill="horizontal"
-              >
-                <Text size="xxsmall" color="dark" textAlign="end">
-                  <FormattedMessage
-                    {...messages.hiNote}
-                    values={{
-                      hiLabel: intl.formatMessage(
-                        rootMessages.labels.hiCountry,
-                      ),
-                    }}
-                  />
-                </Text>
-              </Box>
-            </ContentMaxWidth>
-          </ContentContainer>
-        </Styled>
-      )}
-    </ResponsiveContext.Consumer>
+    <Styled hasAside={hasAside}>
+      <ContentContainer direction="column">
+        <ContentMaxWidth hasAside={hasAside}>
+          <Box direction="row" fill="horizontal">
+            <StyledBox
+              pad={{ bottom: 'xsmall', top: 'xsmall' }}
+              align="start"
+              fill="horizontal"
+              hasAside={hasAside}
+              background="white"
+            >
+              <Text size="xxsmall" color="dark" textAlign="start">
+                <FormattedMessage
+                  {...messages.hiNote}
+                  values={{
+                    hiLabel: intl.formatMessage(rootMessages.labels.hiCountry),
+                  }}
+                />
+              </Text>
+            </StyledBox>
+            {hasAside && <Aside />}
+          </Box>
+        </ContentMaxWidth>
+      </ContentContainer>
+    </Styled>
   );
 }
 
 HINote.propTypes = {
   intl: intlShape.isRequired,
   hasAside: PropTypes.bool,
-  theme: PropTypes.object,
 };
 
-export default injectIntl(withTheme(HINote));
+export default injectIntl(HINote);
