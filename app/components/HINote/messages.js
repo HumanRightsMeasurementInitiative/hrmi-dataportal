dc14e33064cc434dcb129a65b38a38d7bd1e0111
Timo Franz <timo@dumpark.com>
2020-04-21 23:14:07 +0200

    add HI note

---
+++ app/components/HINote/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl';
+
+export const scope = 'hrmi.components.HINote';
+
+export default defineMessages({
+  hiNote: {
+    id: `${scope}.hiNote`,
+    defaultMessage:
+      '{hiLabel} High income country. For these countries it is best to use the high income assessment standard.',
+  },
+});
