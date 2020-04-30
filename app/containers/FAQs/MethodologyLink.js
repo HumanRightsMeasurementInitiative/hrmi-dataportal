0a22525ec9bfd5dd86315a3bd842bd8bfb4457f8
Timo Franz <timo@dumpark.com>
2020-04-30 16:26:55 +0200

    more styling and cleanup

--- app/containers/FAQs/MethodologyLink.js
+++ app/containers/FAQs/MethodologyLink.js
@@ -1,7 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Text } from 'grommet';
+import { FormNext, NewWindow } from 'grommet-icons';
 
-export default styled.div`
-  font-size: ${({ theme }) => theme.text.small.size};
+import ButtonTextIcon from 'styled/ButtonTextIcon';
+
+const StyledText = styled(Text)`
+  font-weight: 600;
   line-height: ${({ theme }) => theme.text.small.height};
   margin: 1em 0;
 `;
+
+function MethodologyLink({ external, text, ...rest }) {
+  return (
+    <ButtonTextIcon
+      size="small"
+      margin={{ vertical: 'xxsmall' }}
+      gap={external ? 'small' : '0'}
+      icon={external ? <NewWindow size="medium" /> : <FormNext size="medium" />}
+      label={<StyledText size="small">{text}</StyledText>}
+      {...rest}
+    />
+  );
+}
+MethodologyLink.propTypes = {
+  text: PropTypes.node,
+  external: PropTypes.bool,
+};
+
+export default MethodologyLink;
