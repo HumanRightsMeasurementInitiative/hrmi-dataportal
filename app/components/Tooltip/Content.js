3f743c9d268732d3abf6f38f87bf6d8d7c7eadd8
Timo Franz <timo@dumpark.com>
2020-05-02 14:22:45 +0200

    fix aside tooltip

---
+++ app/components/Tooltip/Content.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Button } from 'grommet';
+import { Close } from 'grommet-icons';
+import styled from 'styled-components';
+
+const ContentWrap = styled.span`
+  width: 100%;
+`;
+
+function Content({ maxWidth, component, text, onClose, inModal, inverse }) {
+  return (
+    <Box
+      pad={{ vertical: 'small', horizontal: 'small' }}
+      background={inverse ? 'white' : 'darker'}
+      style={{ maxWidth }}
+      onClick={evt => {
+        if (evt) evt.preventDefault();
+        if (evt) evt.stopPropagation();
+      }}
+      align={inModal ? 'center' : 'start'}
+    >
+      {onClose && (
+        <Button
+          onClick={evt => {
+            if (evt) evt.preventDefault();
+            if (evt) evt.stopPropagation();
+            onClose();
+          }}
+          margin={{ left: 'auto' }}
+        >
+          <Close color="white" size="large" />
+        </Button>
+      )}
+      <ContentWrap>
+        {component}
+        {!component && <span>{text}</span>}
+      </ContentWrap>
+    </Box>
+  );
+}
+
+Content.propTypes = {
+  maxWidth: PropTypes.string,
+  component: PropTypes.node,
+  text: PropTypes.string,
+  onClose: PropTypes.func,
+  inModal: PropTypes.bool,
+  inverse: PropTypes.bool,
+};
+
+export default Content;
