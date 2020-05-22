525bcda27e2cc0d4028d90a94051d6e47058e6fe
Timo Franz <timo@dumpark.com>
2020-05-22 13:23:29 +0200

    add formatted markdown support

---
+++ app/components/FormattedMarkdown/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { intlShape, injectIntl } from 'react-intl';
+import Markdown from 'react-remarkable';
+
+function FormattedMarkdown({ id, defaultMessage, values, intl }) {
+  return (
+    <Markdown
+      options={{
+        breaks: true,
+      }}
+      source={intl.formatMessage({ id, defaultMessage }, values)}
+    />
+  );
+}
+
+FormattedMarkdown.propTypes = {
+  intl: intlShape,
+  id: PropTypes.string,
+  defaultMessage: PropTypes.string,
+  values: PropTypes.object,
+};
+
+export default injectIntl(FormattedMarkdown);
