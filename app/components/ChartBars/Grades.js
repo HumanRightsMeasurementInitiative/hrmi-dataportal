9ed2c864ff507a00a0171bcfe6f0ca9332548e64
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-06-22 10:09:20 +1200

    shading on bar charts

--- app/components/ChartBars/Grades.js
+++ app/components/ChartBars/Grades.js
@@ -26,7 +26,7 @@ const BGScale = styled.div`
 
 // background: rgba(0, 0, 0, 0.05);
 const BGScaleX = styled.div`
-  background: ${({ min }) => (min > 0 ? 'rgba(0, 0, 0, 0.06)' : 'transparent')};
+  background: rgba(0, 0, 0, 0.04);
   /* border-left: 1px solid rgba(0, 0, 0, 0.2); */
   position: absolute;
   right: 0;
