e98df718888db764363dbe93096fde1bfe98782a
Timo Franz <timo@dumpark.com>
2020-06-21 17:05:48 +0200

    turn country label in component #161

--- app/utils/countries.js
+++ app/utils/countries.js
@@ -1,5 +1,4 @@
 import { COLUMNS, INCOME_GROUPS } from 'containers/App/constants';
-import rootMessages from 'messages';
 
 export const isCountryHighIncome = country =>
   country[COLUMNS.COUNTRIES.HIGH_INCOME] === '1';
@@ -37,37 +36,4 @@ export const isCountryFeatured = (country, filter, featuredCountries) => {
     [],
   );
   return featured.length > 0 && ['any', ...featured].indexOf(filter[0]) > -1;
-};
-
-export const getCountryLabel = (
-  intl,
-  country,
-  showHILabel = true,
-  showGovRespondentsLabel = true,
-) => {
-  if (!country) return '';
-
-  let label = '';
-  if (rootMessages.countries[country[COLUMNS.COUNTRIES.CODE]]) {
-    label = intl.formatMessage(
-      rootMessages.countries[country[COLUMNS.COUNTRIES.CODE]],
-    );
-  } else {
-    console.log(
-      'Country code not in language files:',
-      country[COLUMNS.COUNTRIES.CODE],
-    );
-    label = country[COLUMNS.COUNTRIES.CODE];
-  }
-  if (showHILabel && isCountryHighIncome(country)) {
-    label = `${label}${intl.formatMessage(rootMessages.labels.hiCountryWrap, {
-      hiLabel: intl.formatMessage(rootMessages.labels.hiCountry),
-    })}`;
-  }
-  if (showGovRespondentsLabel && hasCountryGovRespondents(country)) {
-    label = `${label} <sup>${intl.formatMessage(
-      rootMessages.labels.govResponseCountry,
-    )}</sup>`;
-  }
-  return label;
 };
