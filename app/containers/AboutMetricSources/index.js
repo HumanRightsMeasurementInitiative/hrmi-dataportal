79b9fcdd5007c734444b8ebb2c0e913185d2fa5c
Timo Franz <timo@dumpark.com>
2020-06-22 19:04:57 +0200

    show country level sources in metric panel #89

--- app/containers/AboutMetricSources/index.js
+++ app/containers/AboutMetricSources/index.js
@@ -16,7 +16,10 @@ import { Box, Text, Button } from 'grommet';
 
 import { STANDARDS, INDICATORS } from 'containers/App/constants';
 
-import { getESRIndicators } from 'containers/App/selectors';
+import {
+  getESRIndicators,
+  getAlternativeIndicatorCountrySources,
+} from 'containers/App/selectors';
 import { loadDataIfNeeded } from 'containers/App/actions';
 
 import UL from 'styled/UL';
@@ -32,7 +35,7 @@ const StyledButton = styled(Button)`
   text-decoration: underline;
 `;
 
-const DEPENDENCIES = ['esrIndicators'];
+const DEPENDENCIES = ['esrIndicators', 'sources'];
 
 const expandSource = (source, intl) => {
   const sourcesAsArray = source.split(',');
@@ -60,6 +63,7 @@ export function AboutMetricSources({
   onLoadData,
   onSelectMetric,
   intl,
+  countrySources,
 }) {
   useEffect(() => {
     // kick off loading of data
@@ -111,31 +115,57 @@ export function AboutMetricSources({
   }
   const isAggregateMetric =
     metricType === 'rights' || metricType === 'dimensions';
-  const sources =
+  const sourceCodes =
     indicatorInfo &&
     indicatorInfo.source_codes &&
     indicatorInfo.source_codes.split(',');
+  // prettier-ignore
   return (
     <Box>
-      {metricType === 'indicators' && indicatorInfo && sources.length === 1 && (
-        <>
-          {rootMessages.sources[sources[0]] && (
-            <FormattedMessage {...rootMessages.sources[sources[0]]} />
-          )}
-          {!rootMessages.sources[sources[0]] && sources[0]}
-        </>
+      {metricType === 'indicators' &&
+        indicatorInfo &&
+        (!countrySources || countrySources.length === 0) && (
+          <>
+            {sourceCodes.length === 1 && (
+              <>
+                {rootMessages.sources[sourceCodes[0]] && (
+                  <FormattedMessage {...rootMessages.sources[sourceCodes[0]]} />
+                )}
+                {!rootMessages.sources[sourceCodes[0]] && sourceCodes[0]}
+              </>
+            )}
+            {sourceCodes.length > 1 && (
+              <StyledUL>
+                {sourceCodes.map(source => (
+                  <li key={source}>
+                    {rootMessages.sources[source] && (
+                      <FormattedMessage {...rootMessages.sources[source]} />
+                    )}
+                    {!rootMessages.sources[source] && source}
+                  </li>
+                ))}
+              </StyledUL>
+            )}
+          </>
       )}
-      {metricType === 'indicators' && indicatorInfo && sources.length > 1 && (
-        <StyledUL>
-          {sources.map(source => (
-            <li key={source}>
-              {rootMessages.sources[source] && (
-                <FormattedMessage {...rootMessages.sources[source]} />
-              )}
-              {!rootMessages.sources[source] && source}
-            </li>
-          ))}
-        </StyledUL>
+      {metricType === 'indicators' && countrySources && countrySources.length > 0 && (
+        <>
+          <Text weight="600"><FormattedMessage {...messages.nationalSources} /></Text>
+          {countrySources.length === 1 && (
+            <span>
+              {countrySources[0].description || countrySources[0].source_code}
+            </span>
+          )}
+          {countrySources.length > 1 && (
+            <StyledUL>
+              {countrySources.map(s => (
+                <li key={s.source_code}>
+                  {s.description || s.source_code}
+                </li>
+              ))}
+            </StyledUL>
+          )}
+        </>
       )}
       {isAggregateMetric && indicators && (
         <div>
@@ -182,13 +212,27 @@ AboutMetricSources.propTypes = {
   onSelectMetric: PropTypes.func,
   onLoadData: PropTypes.func.isRequired,
   allIndicators: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
+  sources: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
+  countrySources: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
   intl: intlShape.isRequired,
+  countryCode: PropTypes.string,
+  dateRange: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
   allIndicators: (state, { metric }) => {
     if (metric.metricType !== 'indicators' && metric.type === 'esr') {
       return getESRIndicators(state);
+    }
+    return false;
+  },
+  countrySources: (state, { countryCode, metric, dateRange }) => {
+    if (metric.metricType === 'indicators') {
+      return getAlternativeIndicatorCountrySources(state, {
+        countryCode,
+        metricCode: metric.code,
+        dateRange,
+      });
     }
     return false;
   },
