1fb4b71365670ba42743c8a2a9b4de120a8f34fa
Timo Franz <timo@dumpark.com>
2020-06-22 15:05:38 +0200

    also show sources for dimension on country-metric-overlays #84

--- app/containers/PathCountry/index.js
+++ app/containers/PathCountry/index.js
@@ -236,7 +236,7 @@ export function PathCountry({
       onSetAsideLayer({
         type: 'aboutMetric',
         background: `${dimension || code}Active`,
-        showSources: dimension === 'esr',
+        showSources: dimension === 'esr' || code === 'esr',
         key: code,
         code,
         countryScoreMsg: getScoreMsg(
