91564ffcddeea3610df1186e90b76947f01e066a
bsides44 <maddog.star@gmail.com>
2020-07-22 15:46:03 +1200

    buttontexticonwide color working

--- app/styled/ButtonTextIconWide.js
+++ app/styled/ButtonTextIconWide.js
@@ -1,69 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { FormNext } from 'grommet-icons';
 
-function buttonContent(
-  label,
-  size,
-  icon,
-  hasIcon,
-  iconSize,
-  secondary,
-  reverse,
-) {
-  let iSize = iconSize;
-  if (!iSize) {
-    if (size === 'medium') iSize = 'large';
-    if (size === 'large') iSize = 'xlarge';
-  }
-  const textIcon =
-    icon ||
-    (hasIcon && (
-      // todo access colour
-      <FormNext color="#ffffff" size={iSize} />
-    ));
-
-  return reverse ? (
-    <p style={{ display: 'flex', justifyContent: 'space-between' }}>
-      {label} {textIcon}
-    </p>
-  ) : (
-    <p style={{ display: 'flex', justifyContent: 'space-between' }}>
-      {textIcon} {label}
-    </p>
-  );
-}
+const StyledButton = styled.button`
+  align-self: start;
+  font-weight: 600;
+  background-color: #262262;
+  font-size: 18px;
+  padding: 0 10px;
+  margin: 5px 0;
+  width: 100%;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
 
 export function ButtonTextIconWide({
   onClick,
   icon,
   hasIcon,
   label,
-  secondary,
-  size = 'medium',
   iconSize,
   reverse = true,
 }) {
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      style={{
-        alignSelf: 'start',
-        fontWeight: 600,
-        backgroundColor: '#262262',
-        fontSize: '18px',
-        padding: '0 10px',
-        margin: '5px 0',
-        width: '100%',
-        borderRadius: '5px',
-        border: 'none',
-        outline: 'none',
-        cursor: 'pointer',
-      }}
-    >
-      {buttonContent(label, size, icon, hasIcon, iconSize, secondary, reverse)}
-    </button>
+    <StyledButton type="button" onClick={onClick}>
+      <p style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {reverse && label}
+        {icon ||
+          (hasIcon && <FormNext color="white" size={iconSize || 'large'} />)}
+        {!reverse && label}
+      </p>
+    </StyledButton>
   );
 }
 
@@ -71,10 +41,8 @@ ButtonTextIconWide.propTypes = {
   onClick: PropTypes.func,
   icon: PropTypes.node,
   hasIcon: PropTypes.bool,
-  secondary: PropTypes.bool,
   reverse: PropTypes.bool,
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  size: PropTypes.string,
   iconSize: PropTypes.string,
 };
 
