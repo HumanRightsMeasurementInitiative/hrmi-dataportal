87c56446130a793442e9b14dbb0d3fbcdb7422e8
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-07-28 15:58:21 +1200

    example of manual html for AFG with og tag inserted

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -86,6 +86,23 @@ module.exports = require('./webpack.base.babel')({
       },
       inject: true,
     }),
+    new HtmlWebpackPlugin({
+      filename: 'AFG.html',
+      template: 'app/AFG.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
 
     // Put it in the end to capture all the HtmlWebpackPlugin's
     // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
