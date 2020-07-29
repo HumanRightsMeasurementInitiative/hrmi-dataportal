98b4e08215e80001726b3ecd703c41e212500615
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-07-30 10:23:47 +1200

    add rights and indicators pages as their own html for sharing

--- internals/webpack/html-plugins.js
+++ internals/webpack/html-plugins.js
@@ -6,15 +6,42 @@ const keys = require('lodash/keys');
 
 const langJSON = require('../../app/translations/en.json');
 
+const getMessageName = data => mapKeys(data, (v, k) => k.split('.')[2]);
+
 const getCountries = flow([
   data => pickBy(data, (v, k) => k.includes('hrmi.countries')),
-  data => mapKeys(data, (v, k) => k.split('.')[2]),
+  getMessageName,
+]);
+
+const getRights = flow([
+  data =>
+    pickBy(
+      data,
+      (v, k) =>
+        k.includes('hrmi.rights.') &&
+        !k.includes('job') &&
+        !k.includes('union'), // N.B. note the dot following hrmi.rights.
+    ),
+  getMessageName,
+]);
+
+const getIndicators = flow([
+  data =>
+    pickBy(
+      data,
+      (v, k) => k.includes('hrmi.indicators.'), // N.B. note the dot following hrmi.indicators.
+    ),
+  getMessageName,
 ]);
 
 const countries = getCountries(langJSON);
+const rights = getRights(langJSON);
+const indicators = getIndicators(langJSON);
+
+const pages = { ...countries, ...rights, ...indicators };
 
 // webpack
-const htmlPlugins = keys(countries).map(
+const htmlPlugins = keys(pages).map(
   code =>
     new HtmlWebpackPlugin({
       filename: `${code}.html`,
