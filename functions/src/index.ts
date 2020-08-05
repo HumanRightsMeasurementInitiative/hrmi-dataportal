68103241b68949cdeacf225767b8929807238b92
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-08-06 11:28:54 +1200

    add timing logs to pdf function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,19 +15,27 @@ export async function printPDF({
   route = 'http://localhost:3000/en/country/FJI?as=core',
   path
 }: printPDFOptions) {
+  console.time('puppeteer')
+  console.time('puppeteer launch and newPage')
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   // need to emulate print media explicitly for background images
   // see https://github.com/puppeteer/puppeteer/issues/436#issuecomment-564008025
   await page.emulateMediaType('print');
+  console.timeEnd('puppeteer launch and newPage')
+  console.time('puppeteer goto')
   await page.goto(route, {
     waitUntil: 'networkidle2',
   });
+  console.timeEnd('puppeteer goto')
+  console.time('puppeteer page.pdf')
   const result = await page.pdf({
     path,
     format: 'A4',
     printBackground: true,
   });
+  console.timeEnd('puppeteer page.pdf')
+  console.timeEnd('puppeteer')
 
   await browser.close();
 
