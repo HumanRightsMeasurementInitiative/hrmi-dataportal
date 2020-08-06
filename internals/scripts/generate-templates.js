1921cdaa2a0278414a04621d6bde263a195e7ebc
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-08-07 10:10:07 +1200

    add generated pdfs and script to recreate

--- internals/scripts/generate-templates.js
+++ internals/scripts/generate-templates.js
@@ -1,40 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio')
-const pickBy = require('lodash/pickBy')
-const flow = require('lodash/flow')
-const mapKeys = require('lodash/mapKeys')
 const map = require('lodash/map')
 const keys = require('lodash/keys')
 
+const { getCountries, getIndicators, getRights } = require('./helpers/generate-files')
+
 const firebaseJSON = require('../../firebase.json')
 const langJSON = require('../../app/translations/en.json')
-
-const getMessageName = (data) => mapKeys(data, (v, k) => {
-  return k.split('.')[2]
-})
-
-// rewrites
-const getCountries = flow([
-  (data) => pickBy(data, (v, k) => {
-    return k.includes('hrmi.countries')
-  }),
-  getMessageName
-])
-
-const getRights = flow([
-  (data) => pickBy(data, (v, k) => {
-    return k.includes('hrmi.rights.') && !k.includes('job') && !k.includes('union') // N.B. note the dot following hrmi.rights.
-  }),
-  getMessageName
-])
-
-const getIndicators = flow([
-  (data) => pickBy(data, (v, k) => {
-    return k.includes('hrmi.indicators.') // N.B. note the dot following hrmi.indicators.
-  }),
-  getMessageName
-])
 
 const countries = getCountries(langJSON)
 const rights = getRights(langJSON)
