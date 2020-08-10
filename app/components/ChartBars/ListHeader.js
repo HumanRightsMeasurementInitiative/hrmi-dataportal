4ac1af8887969c2a00de15c18a8f171ad08f2240
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-08-10 13:25:49 +1200

    add label to top of CPR charts

--- app/components/ChartBars/ListHeader.js
+++ app/components/ChartBars/ListHeader.js
@@ -80,13 +80,11 @@ export function ListHeader({
                 <Text size="xsmall" style={{ transform: 'translateX(-50%)' }}>
                   0
                 </Text>
-                {metric && metric.type === 'esr' && (
-                  <Text size="xsmall" weight={500} textAlign="center">
-                    <FormattedMessage
-                      {...rootMessages.labels.xAxis[benchmark || 'cpr']}
-                    />
-                  </Text>
-                )}
+                <Text size="xsmall" weight={500} textAlign="center">
+                  <FormattedMessage
+                    {...rootMessages.labels.xAxis[benchmark || 'cpr']}
+                  />
+                </Text>
                 <Text size="xsmall" style={{ transform: 'translateX(50%)' }}>
                   {metric.type === 'esr' || metric.metricType === 'indicators'
                     ? '100%'
