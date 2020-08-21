8f75a14d0cd3fede9d1c46923f0c86164675e509
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-08-21 15:28:26 +1200

    fixes for pdf generation, including fonts in header / footer, page break whitespace

--- internals/scripts/generate-pdfs.js
+++ internals/scripts/generate-pdfs.js
@@ -33,14 +33,21 @@ async function printPDF({
       waitUntil: 'networkidle2',
     });
 
+    // <style> #header {padding: 0 !important} p, span { font-family: 'Source Sans Pro', sans-serif; font-size: 10px; color: #262064;}</style><div style="width: 100%; display: flex; flex-direction: row; justify-content: space-between;margin-left: 35px; margin-right: 35px; margin-top: 5px; margin-bottom: 0;"> <img src=${logo} alt="logo" style="width: 120px"></img> <p style="font-weight: 600">${langFile['hrmi.pdf.subtitle']} ${langFile[`hrmi.countries.${country}`]}, ${currentYear}</p> </div>
+
+    // <style> @font-face {font-family: SourceSansPro;src:url(../../fonts/SourceSansPro-Regular.ttf) format("truetype");} p, span {font-family: SourceSansPro !important; font-size: 10px; color: #262064;} #footer { padding: 0 !important; } </style><div style="height: 40px; width: 100%; background-color: #d3d3d3; -webkit-print-color-adjust: exact; display: flex; flex-direction: row; justify-content: space-around;"> <p style="font-weight: 600;">  ©HRMI 2020 </p> <a href="http://rightstracker.org" style="text-decoration: none; color: unset"> <p>rightstracker.org </p> </a> <p style="font-weight: 600;"> Page <span class="pageNumber"></span>/<span class="totalPages"></span> </p></div>
+
+    const headerFooterStyle = `<style>@font-face{font-family:'Source Sans Pro';src:url(../../fonts/SourceSansPro-Regular.ttf) format("truetype");} #header { padding: 0 !important; } #footer { padding: 0 !important; } p, span { font-family: 'Source Sans Pro', sans-serif; font-size: 10px; color: #262064;}</style>`
+
+
     try {
       await page.pdf({
         path: `pdfs/${lang}-${country}.pdf`,
         format: 'A4',
         printBackground: true,
         displayHeaderFooter: true,
-        headerTemplate: `<style> #header {padding: 0 !important} p, span { font-family: SourceSansPro !important; font-size: 10px; color: #262064;}</style><div style="width: 100%; display: flex; flex-direction: row; justify-content: space-between;margin-left: 35px; margin-right: 35px; margin-top: 5px; margin-bottom: 0;"> <img src=${logo} alt="logo" style="width: 120px"></img> <p style="font-weight: 600">${langFile['hrmi.pdf.subtitle']} ${langFile[`hrmi.countries.${country}`]}, ${currentYear}</p> </div>`,
-        footerTemplate: `<style> @font-face {font-family: SourceSansPro;src:url(../../fonts/SourceSansPro-Regular.ttf) format("truetype");} p, span {font-family: SourceSansPro !important; font-size: 10px; color: #262064;} #footer { padding: 0 !important; } </style><div style="height: 40px; width: 100%; background-color: #d3d3d3; -webkit-print-color-adjust: exact; display: flex; flex-direction: row; justify-content: space-around;"> <p style="font-weight: 600;">  ©HRMI 2020 </p> <a href="http://rightstracker.org" style="text-decoration: none; color: unset"> <p>rightstracker.org </p> </a> <p style="font-weight: 600;"> Page <span class="pageNumber"></span>/<span class="totalPages"></span> </p></div>`,
+        headerTemplate: `${headerFooterStyle} <div style="font-family: 'Source Sans Pro', sans-serif; width: 100%; display: flex; flex-direction: row; justify-content: space-between;margin-left: 35px; margin-right: 35px; margin-top: 5px; margin-bottom: 0;"> <img src=${logo} alt="logo" style="width: 120px"></img> <p style="font-weight: 600">${langFile['hrmi.pdf.subtitle']} ${langFile[`hrmi.countries.${country}`]}, ${currentYear}</p> </div>`,
+        footerTemplate: `${headerFooterStyle} <div style="font-family: 'Source Sans Pro', sans-serif; height: 40px; width: 100%; background-color: #d3d3d3; -webkit-print-color-adjust: exact; display: flex; flex-direction: row; justify-content: space-around;"> <p style="font-weight: 600;">  ©HRMI 2020 </p> <a href="http://rightstracker.org" style="text-decoration: none; color: unset"> <p>rightstracker.org </p> </a> <p style="font-weight: 600;"> Page <span class="pageNumber"></span>/<span class="totalPages"></span> </p></div>`,
         margin: {
           top: "55px",
           bottom: "76px",
@@ -75,8 +82,9 @@ async function printPDF({
   const timePrintPDF = process.hrtime()
   await printPDF({
     // countries: keys(getCountries(enJSON)),
-    countries: ['NZL'],
-    languages: ['en', 'fr', 'es', 'pt']
+    countries: ['CAN'],
+    // languages: ['en', 'fr', 'es', 'pt']
+    languages: ['fr', 'es', 'pt']
   });
 
   console.log(`generate-pdfs done, timePrintPDF: ${process.hrtime(timePrintPDF)[0]}.${process.hrtime(timePrintPDF)[1]} seconds`)
