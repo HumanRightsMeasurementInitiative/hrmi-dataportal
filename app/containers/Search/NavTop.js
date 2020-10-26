00d0a6f1f325a39b3943f8e5914f2c7ab2b32433
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-10-27 09:42:35 +1300

    convert NavTop to FC to use useRef for input focusing with grommet

--- app/containers/Search/NavTop.js
+++ app/containers/Search/NavTop.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Box, Button } from 'grommet';
@@ -27,85 +27,73 @@ const Top = styled.div`
   height: ${({ theme }) => theme.navTop};
 `;
 
-class NavTop extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-  }
+function NavTop({ search, onClose, onSearch, placeholder = 'search', size }) {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    if (isMinSize(size, 'medium')) {
+      inputRef.current.focus();
+    }
+  });
 
-  componentDidMount() {
-    if (this.textInput && this.textInput.current)
-      this.textInput.current.focus();
-  }
-
-  render() {
-    const {
-      search,
-      onClose,
-      onSearch,
-      placeholder = 'search',
-      size,
-    } = this.props;
-    return (
-      <Top>
+  return (
+    <Top>
+      <Box
+        pad={{
+          left: isMaxSize(size, 'sm') ? 'small' : 'medium',
+          vertical: 'small',
+          right: isMaxSize(size, 'sm') ? 'small' : 'medium',
+        }}
+        direction="row"
+        fill="vertical"
+        align="center"
+        responsive={false}
+      >
         <Box
-          pad={{
-            left: isMaxSize(size, 'sm') ? 'small' : 'medium',
-            vertical: 'small',
-            right: isMaxSize(size, 'sm') ? 'small' : 'medium',
-          }}
+          background="white"
           direction="row"
-          fill="vertical"
           align="center"
-          responsive={false}
+          round="xlarge"
+          height={isMaxSize(size, 'sm') ? '26px' : '32px'}
+          pad={{ horizontal: 'ms', vertical: 'xsmall' }}
+          fill="horizontal"
         >
-          <Box
-            background="white"
-            direction="row"
-            align="center"
-            round="xlarge"
-            height={isMaxSize(size, 'sm') ? '26px' : '32px'}
-            pad={{ horizontal: 'ms', vertical: 'xsmall' }}
-            fill="horizontal"
-          >
-            <TextInput
-              plain
-              value={search}
-              onChange={evt => evt && evt.target && onSearch(evt.target.value)}
-              placeholder={placeholder}
-              ref={isMinSize(size, 'medium') && this.textInput}
+          <TextInput
+            plain
+            value={search}
+            onChange={evt => evt && evt.target && onSearch(evt.target.value)}
+            placeholder={placeholder}
+            ref={inputRef}
+          />
+          {search && search.length > 0 && (
+            <Button
+              onClick={() => onSearch('')}
+              icon={
+                <Close
+                  color="dark"
+                  size={isMaxSize(size, 'sm') ? 'small' : 'medium'}
+                />
+              }
+              style={{ padding: '0' }}
             />
-            {search && search.length > 0 && (
-              <Button
-                onClick={() => onSearch('')}
-                icon={
-                  <Close
-                    color="dark"
-                    size={isMaxSize(size, 'sm') ? 'small' : 'medium'}
-                  />
-                }
-                style={{ padding: '0' }}
-              />
-            )}
-            {(!search || search.length === 0) && (
-              <Search bordersize="small" bordercolor="dark" color="dark" />
-            )}
-          </Box>
-          {isMaxSize(size, 'sm') && (
-            <Box
-              pad={{ vertical: 'xsmall' }}
-              flex={{ shrink: 0 }}
-              margin={{ left: 'small' }}
-            >
-              <StyledButtonIcon onClick={() => onClose()}>
-                <Close color="white" size="large" />
-              </StyledButtonIcon>
-            </Box>
+          )}
+          {(!search || search.length === 0) && (
+            <Search bordersize="small" bordercolor="dark" color="dark" />
           )}
         </Box>
-      </Top>
-    );
-  }
+        {isMaxSize(size, 'sm') && (
+          <Box
+            pad={{ vertical: 'xsmall' }}
+            flex={{ shrink: 0 }}
+            margin={{ left: 'small' }}
+          >
+            <StyledButtonIcon onClick={() => onClose()}>
+              <Close color="white" size="large" />
+            </StyledButtonIcon>
+          </Box>
+        )}
+      </Box>
+    </Top>
+  );
 }
 
 NavTop.propTypes = {
