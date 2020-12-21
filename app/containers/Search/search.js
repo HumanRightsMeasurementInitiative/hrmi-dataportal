f5a3c03403330ab8e74c97a06622b40cdfa1842e
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-12-22 11:57:40 +1300

    different styling for sub labels of dimensions in rights menu

--- app/containers/Search/search.js
+++ app/containers/Search/search.js
@@ -51,6 +51,9 @@ export const prepMetrics = (metrics, metricType, search, intl) =>
     .map(m => ({
       code: m.key,
       label: intl.formatMessage(rootMessages[metricType][m.key]),
+      sub: rootMessages[metricType][`${m.key}-sub`]
+        ? intl.formatMessage(rootMessages[metricType][`${m.key}-sub`])
+        : null,
       right: m.right,
     }))
     .filter(m => filterMetric(m, search));
