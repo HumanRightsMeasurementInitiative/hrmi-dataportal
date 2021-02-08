84b8d6e12dc3038e4c7e9f832d067980b7cd8882
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2021-02-08 16:36:59 +1300

    fix for FAQ aside stickyness

--- app/components/Aside/index.js
+++ app/components/Aside/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Box, ResponsiveContext } from 'grommet';
 import styled from 'styled-components';
@@ -13,23 +13,32 @@ const Styled = styled(Box)`
   }
   @media (min-width: ${({ theme }) => theme.breakpointsMin.large}) {
     position: sticky;
-    top: 140px;
+    top: ${({ top }) => `-${top}px`};
     padding-right: ${({ theme, image }) =>
     image ? 0 : theme.global.edgeSize.xlarge};
   }
 `;
 
 function Aside({ content, image, active, children, ...other }) {
+  const [top, setTop] = useState(0);
+  const asideEl = useRef(null);
+  useLayoutEffect(() => {
+    if (asideEl.current) {
+      setTop(asideEl.current.clientHeight);
+    }
+  }, [asideEl]);
   // prettier-ignore
   return (
     <ResponsiveContext.Consumer>
       {size => (
         <Styled
+          ref={asideEl}
           width={getAsideWidth(size)}
           direction="column"
           flex={{ shrink: 0 }}
           fill="vertical"
           image={image}
+          top={top}
           {...other}
         >
           {content
