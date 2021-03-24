2ef793594421f66a7831a6de8f7f8a2e6de8abb5
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2021-03-24 17:20:35 +1300

    update fonts script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install wget
-wget -O ~/Downloads/Source_Sans_Pro.zip https://fonts.google.com/download?family=Source%20Sans%20Pro
+# sudo apt-get install wget
+# wget -O ~/Downloads/Source_Sans_Pro.zip https://fonts.google.com/download?family=Source%20Sans%20Pro
 cd /usr/share/fonts
 sudo mkdir googlefonts
 cd googlefonts
