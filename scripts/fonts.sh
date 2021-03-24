1aaf0ffdcc71b266e2d0f8eeb805a71091c69f37
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2021-03-24 17:27:10 +1300

    update fonts script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # sudo apt-get install wget
-wget -O ./Source_Sans_Pro.zip https://fonts.google.com/download?family=Source%20Sans%20Pro
+wget -O ~/Downloads/Source_Sans_Pro.zip https://fonts.google.com/download?family=Source%20Sans%20Pro
+pwd
+ls ~/Downloads
 cd /usr/share/fonts
 sudo mkdir googlefonts
 cd googlefonts
-sudo unzip -d . ./Source_Sans_Pro.zip
+sudo unzip -d . ~/Downloads/Source_Sans_Pro.zip
 sudo chmod -R --reference=/usr/share/fonts/opentype /usr/share/fonts/googlefonts
 sudo fc-cache -fv
 fc-match SourceSansPro
