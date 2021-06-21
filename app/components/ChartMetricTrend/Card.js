06178a35d3352968d15a7efa1f80ff9be7af1177
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2021-06-21 18:06:08 +1000

    boost small multiples size up, testing it out

--- app/components/ChartMetricTrend/Card.js
+++ app/components/ChartMetricTrend/Card.js
@@ -6,8 +6,8 @@ export default styled(Box)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 240px;
-  min-height: 240px;
+  width: 300px;
+  min-height: 300px;
   background: white;
   position: relative;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
